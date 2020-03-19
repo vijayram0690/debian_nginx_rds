720350ebf807462d9edf0cf95998e47687a0bb99
vijayram0690 <vijay.ram0690@gmail.com>
2020-03-19 12:02:56 +0530

    output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "address" {
+ value = aws_elb.web.dns_name
+}
+
+output "endpoint" {
+  description = "The connection endpoint in address:port format."
+  value       = aws_db_instance.rds.endpoint
+}
+
+output "master_password" {
+  description = "The random master password assigned to the database."
+  sensitive   = true
+  value       = random_string.master_password.result
+}
+
+output "security_group_id" {
+  description = "The ID of the database security group."
+  value       = aws_security_group.rds.id
+}
